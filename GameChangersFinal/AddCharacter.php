73012551a927d66f146cab87e92ac5e62c41e177
SpencerBuff6633 <61481885+SpencerBuff6633@users.noreply.github.com>
2022-08-21 18:18:03 -0600

    update add character

--- GameChangersFinal/AddCharacter.php
+++ GameChangersFinal/AddCharacter.php
@@ -13,10 +13,15 @@ include_once "./Wrappers/menu.php";
 
 <form method="post" action="">
     <fieldset>
-        <!--<label for="gameName">Game Name:</label><input type="text" name="gameName" size="20" /><br />
-        <label for="releaseDate">Release Date:</label><input type="text" name="releaseDate" size="20" /><br />
-        <label for="genre">Genre:</label><input type="text" name="genre" size="20" /><br />
-        <label for="rating">Rating:</label><input type="text" name="rating" size="20" /><br />-->
+        <label for="characterName">Name:</label><input type="text" name="characterName" size="20" /><br />
+        <label for="characterGender">Gender:</label><input type="text" name="characterGender" size="20" /><br />
+        <label for="characterHeight">Height:</label><input type="text" name="characterHeight" size="20" /><br />
+        <label for="characterWeight">Weight:</label><input type="text" name="characterWeight" size="20" /><br />
+        <label for="characterAge">Age:</label><input type="text" name="characterAge" size="20" /><br />
+        <label for="characterHair">Hair Details:</label><input type="text" name="characterHair" size="20" /><br />
+        <label for="characterEye">Eye Color</label><input type="text" name="characterEyes" size="20" /><br />
+        <label for="characterRace">Race:</label><input type="text" name="characterRace" size="20" /><br />
+        <label for="characterDetails">Additional Details:</label><input type="text" name="characterDetails" size="20" /><br />
     </fieldset>
     <input class="btn" type="submit" name="CharSubmit" value="Add Character" />
 </form>
