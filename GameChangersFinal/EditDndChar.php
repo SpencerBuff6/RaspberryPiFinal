41977999d649a30d3d9a08df0ca4c911052ee55b
Dalton Cobb <DCobb@student.nemount.edu>
2022-08-21 18:44:54 -0600

    editdndcharacter

--- GameChangersFinal/EditDndChar.php
+++ GameChangersFinal/EditDndChar.php
@@ -14,10 +14,18 @@ include_once "./Wrappers/menu.php";
 
 <form method="post" action="">
     <fieldset>
-        <!--<label for="gameName">Game Name:</label><input type="text" name="gameName" value="<?php echo $_SESSION['games'][$_SESSION['EditIds'][0]][0] ?>" size="20" /><br />
-        <label for="releaseDate">Release Date:</label><input type="text" name="releaseDate" value="<?php echo $_SESSION['games'][$_SESSION['EditIds'][0]][1] ?>" size="20" /><br />
-        <label for="genre">Genre:</label><input type="text" name="genre" value="<?php echo $_SESSION['games'][$_SESSION['EditIds'][0]][2] ?>" size="20" /><br />
-        <label for="rating">Rating:</label><input type="text" name="rating" value="<?php echo $_SESSION['games'][$_SESSION['EditIds'][0]][3] ?>" size="20" /><br />-->
+        <label for="dndcharacterName">dndName:</label><input type="text" name="dndcharacterName" value="<?php echo $_SESSION['dndCharacter'][$_SESSION['EditIds'][0]][0] ?>" size="20" /><br />
+        <label for="dndcharacterRace">dndRace:</label><input type="text" name="dndcharacterRace" value="<?php echo $_SESSION['dndCharacter'][$_SESSION['EditIds'][0]][1] ?>" size="20" /><br />
+        <label for="dndcharacterClass">dndClass:</label><input type="text" name="dndcharacterClass" value="<?php echo $_SESSION['dndCharacter'][$_SESSION['EditIds'][0]][2] ?>" size="20" /><br />
+        <label for="dndcharacterLevel">dndLevel:</label><input type="text" name="dndcharacterLevel" value="<?php echo $_SESSION['dndCharacter'][$_SESSION['EditIds'][0]][3] ?>" size="20" /><br />
+        <label for="dndcharacterMaxHealth">dndMaxHealth:</label><input type="text" name="dndcharacterMaxHealth" value="<?php echo $_SESSION['dndCharacter'][$_SESSION['EditIds'][0]][4] ?>" size="20" /><br />
+        <label for="dndcharacterStrength">dndStrength:</label><input type="text" name="dndcharacterStrength" value="<?php echo $_SESSION['dndCharacter'][$_SESSION['EditIds'][0]][5] ?>" size="20" /><br />
+        <label for="dndcharacterDexterity">dndDexterity:</label><input type="text" name="dndcharacterDexterity" value="<?php echo $_SESSION['dndCharacter'][$_SESSION['EditIds'][0]][6] ?>" size="20" /><br />
+        <label for="dndcharacterConstitution">dndConstitution:</label><input type="text" name="dndcharacterConstitution" value="<?php echo $_SESSION['dndCharacter'][$_SESSION['EditIds'][0]][7] ?>" size="20" /><br />
+        <label for="dndcharacterIntelligence">dndIntelligence:</label><input type="text" name="dndcharacterIntelligence" value="<?php echo $_SESSION['dndCharacter'][$_SESSION['EditIds'][0]][8] ?>" size="20" /><br />
+        <label for="dndcharacterWisdom">dndWisdom:</label><input type="text" name="dndcharacterWisdom" value="<?php echo $_SESSION['dndCharacter'][$_SESSION['EditIds'][0]][9] ?>" size="20" /><br />
+        <label for="dndcharacterChrisma">dndChrisma:</label><input type="text" name="dndcharacterChrisma" value="<?php echo $_SESSION['dndCharacter'][$_SESSION['EditIds'][0]][10] ?>" size="20" /><br />
+        <label for="dndcharacterAdditionalDetails">dndAdditionalDetails:</label><input type="text" name="dndcharacterAdditionalDetails" value="<?php echo $_SESSION['dndCharacter'][$_SESSION['EditIds'][0]][11] ?>" size="20" /><br />
     </fieldset>
     <input class="btn" type="submit" name="DndCharSubmit" value="Edit D&D Character" />
 </form>
